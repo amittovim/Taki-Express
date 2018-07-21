751825ab6759f3ca56e5be1d565a0701ebac1a18
amit tovim <amit.tovim@gmail.com>
2018-07-21 20:28:28 +0300

    wip

--- src/app/serverGame/components/game-list.component.js
+++ src/app/serverGame/components/game-list.component.js
@@ -4,23 +4,21 @@ import GameListItem from './game-list-item.component';
 export default class GameList extends Component {
     render() {
         if (this.state.gameList.length > 0) {
-            const gameItems = this.state.gameList.map((game) => {
-                debugger;
-                return (
-                    <GameListItem key={game.id + game.name}
-                                  game={game}
-
-                    />
-                );
-            });
             return (
-                <ul className="gameList-component">
-                    {gameItems}
-                </ul>
-            );
+                <div className="game-list-component">
+                    <ul>
+                        {this.state.gameList.map((game) => (
+                            <li key={game.id + game.name}>
+                                        game name : {game.name} <br/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )
         }
-        else return (<div></div>);
-    }
+        return (
+            <div></div>
+        );    }
         constructor(props)
         {
             super(...props);
