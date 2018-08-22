53ed90741094fa249f8c619668d0fb37ea39fbf6
amit tovim <amit.tovim@gmail.com>
2018-08-22 20:29:37 +0300

    wip

--- src/server/logic/consts.js
+++ src/server/logic/consts.js
@@ -1,6 +1,6 @@
-const NUMBER_OF_STARTING_CARDS_IN_PLAYERS_HAND = 2;
+const NUMBER_OF_STARTING_CARDS_IN_PLAYERS_HAND = 8;
 const VISIBLE_CARDS = false;
-const ALL_MOVES_LEGAL = true;
+const ALL_MOVES_LEGAL = false;
 const DEBBUG_MODE = false;
 
 module.exports = {
